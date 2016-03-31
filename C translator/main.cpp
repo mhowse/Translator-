f9bac830fb84c28046d04457848b4af52ca9b25e
mhowse <mikhaila.howse@gmail.com>
2016-03-31 22:05:14 +1300

    starting simple c translator

---
+++ C translator/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: mhowse
+ * This will be a german-english translator. 
+ * It will be somewhat simpler then the java translator.  
+ * Created on 22 March 2016, 10:20 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int days [12]={1,2,3,4,5,6,7,8,9,10,11,12};
+    char*  German [4]= {"alles","die","der","das"};
+    char*  English [4]={"all","the, feminine or plural", "the, masculine","the, neuter"};
+    
+    return 0;
+}
+
+
